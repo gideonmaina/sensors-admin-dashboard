5c945a46dbd9f7eb1e6046dab7f7c5e2bdc3a76c
Gideon Maina <gmainapro@yahoo.com>
2023-03-31 10:21:34 +0300

    simulate maintenance log data

--- src/components/SensorViewComponents/NodeMaintenanceLog.vue
+++ src/components/SensorViewComponents/NodeMaintenanceLog.vue
@@ -1,49 +1,48 @@
 <script setup>
+import logs from "../../simulationdata/maintenancelog.json"
 
-const dates= [
-      {
-        color: 'cyan',
-        date: '30/03/2023',
-      },
-      {
-        color: 'green',
-        date: '02/12/2022',
-      },
-      {
-        color: 'pink',
-        date: '9/06/2022',
-      },
-      {
-        color: 'amber',
-        date: '10/2/2022',
-      },
-      {
-        color: 'orange',
-        date: '04/07/2021',
-      },
-    ]
-
+const logStatus=bool=>bool?"Resolved ✅":"Unresolved 🚨"
 </script>
 
 
 <template>
 
 <v-timeline align="start">
-        <v-timeline-item v-for="(date, i) in dates" :key="i" :dot-color="date.color" size="small">
+        <v-timeline-item v-for="(log, i) in logs" :key="i" :dot-color="log.color" size="small">
             <template v-slot:opposite>
-                <div :class="`pt-1 headline font-weight-bold text-${date.color}`" v-text="date.date"></div>
+                <div :class="`pt-1 headline font-weight-bold text-${log.color}`" v-text="log.date"></div>
             </template>
             <div>
-                <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${date.color}`">
-                    Lorem ipsum
+                <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${log.color}`">
+                    {{logStatus(log.resolved)}}
                 </h2>
                 <div>
+                  <p>{{ log.report }}</p><br>
                     Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
                     convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
                     salutandi sententiae.
+                </div>
+
+                <div class="personnel">
+                  <p>Maintained By:</p>
+                  <span v-for="staff in log.attention" :key="staff">
+                    {{staff }} ,
+                  </span>
                 </div>
             </div>
         </v-timeline-item>
     </v-timeline>
 
 </template>
+
+<style scoped>
+
+.personnel{
+  margin:4px 0;
+}
+.personnel span{
+  font-weight: 600;
+  margin-right: 2px;
+ 
+}
+</style>
---
+++ src/components/avators/BaseAvator.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { defineProps } from 'vue';
+
+
+</script>
+
+
+<template>
+    <slot>
+        <v-avatar color="info">
+            <v-icon icon="mdi-account-circle"></v-icon>
+        </v-avatar>
+    </slot>
+    
+    <slot name="named-avatar">
+        <v-avatar color="info">
+            <v-icon icon="mdi-account-circle"></v-icon>
+        </v-avatar>
+    </slot>
+</template>
