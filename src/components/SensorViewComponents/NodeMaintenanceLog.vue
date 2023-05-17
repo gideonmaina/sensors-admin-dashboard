35e9728e368180db12b4b03d8b9d650ddea70f6f
gideon maina <gmainapro@yahoo.com>
2023-05-17 12:32:50 +0300

    pretiffy node view page

--- src/components/SensorViewComponents/NodeMaintenanceLog.vue
+++ src/components/SensorViewComponents/NodeMaintenanceLog.vue
@@ -1,56 +1,45 @@
 <script setup>
-import logs from "../../simulationdata/maintenancelog.json"
+import logs from "../../simulationdata/maintenancelog.json";
 
-import Avatar from "../avators/BaseAvator.vue"
+import Avatar from "../avators/BaseAvator.vue";
 
-
-
-const logStatus=bool=>bool?"Resolved ✅":"Unresolved 🚨"
+const logStatus = (bool) => (bool ? "Resolved ✅" : "Unresolved 🚨");
 </script>
 
-
 <template>
+  <div class="px-4">
+    <v-timeline align="center" side="end">
+      <v-timeline-item v-for="(log, i) in logs" :key="i" :dot-color="log.color" size="small">
+        <template v-slot:opposite>
+          <div :class="`pt-1 headline font-weight-bold underline underline-offset-4 text-${log.color}`" v-text="log.date">
+          </div>
+        </template>
+        <v-card class="px-2 py-4">
+          <h2 :class="`mt-n1 headline font-weight-bold mb-4 text-${log.color}`">
+            {{ logStatus(log.resolved) }}
+          </h2>
+          <div class="my-4">
+            <h6>{{ log.report }}</h6>
+            <p class="mt-4">
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
+              scaevola imperdiet nec ut, sed euismod convenire principes at. Est
+              et nobis iisque percipit, an vim zril disputando voluptatibus, vix
+              an salutandi sententiae.
+            </p>
+          </div>
+          <div class="flex flex-col gap-2">
+            <p>Maintained By:</p>
 
-<v-timeline align="start">
-        <v-timeline-item v-for="(log, i) in logs" :key="i" :dot-color="log.color" size="small">
-            <template v-slot:opposite>
-                <div :class="`pt-1 headline font-weight-bold text-${log.color}`" v-text="log.date"></div>
-            </template>
-            <div>
-                <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${log.color}`">
-                    {{logStatus(log.resolved)}}
-                </h2>
-                <div>
-                  <p>{{ log.report }}</p><br>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
-                    convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
-                    salutandi sententiae.
-                </div>
-
-                <div class="personnel">
-                  <p>Maintained By:</p>
-                  <span v-for="staff in log.attention" :key="staff">
-                    <Avatar :name="staff">
-                    <template v-slot:named-avatar >
-                   
-                    </template>
-                    </Avatar>
-                  </span>
-                </div>
+            <div class="flex gap-2">
+              <span v-for="staff in log.attention" :key="staff">
+                <Avatar :name="staff">
+                  <template v-slot:named-avatar> </template>
+                </Avatar>
+              </span>
             </div>
-        </v-timeline-item>
+          </div>
+        </v-card>
+      </v-timeline-item>
     </v-timeline>
-
+  </div>
 </template>
-
-<style scoped>
-
-.personnel{
-  margin:4px 0;
-}
-.personnel span{
-  font-weight: 600;
-  margin-right: 2px;
- 
-}
-</style>
--- src/views/NodeView.vue
+++ src/views/NodeView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 // import { useRoute } from "vue-router";
 import NodeDetails from "@/components/SensorViewComponents/NodeDetails.vue"
 import MaintenanceLog from "@/components/SensorViewComponents/NodeMaintenanceLog.vue"
+import EditNode from "@/components/SensorViewComponents/EditNode.vue"
 // const node_id = useRoute().params.node_id;
 const tab = ref(null)
 </script>
@@ -12,7 +13,7 @@ const tab = ref(null)
     <v-tabs v-model="tab" bg-color="#00e5a0">
       <v-tab value="info">Node Info</v-tab>
       <v-tab value="maintenance">Maintenance Log</v-tab>
-      <v-tab value="three">Edit</v-tab>
+      <v-tab value="edit">Edit</v-tab>
     </v-tabs>
 
     <div class="mt-2">
@@ -26,7 +27,7 @@ const tab = ref(null)
           <MaintenanceLog />
         </v-window-item>
 
-        <v-window-item value="three"> Edit Page </v-window-item>
+        <v-window-item value="edit"> <EditNode/> </v-window-item>
       </v-window>
 
     </div>
