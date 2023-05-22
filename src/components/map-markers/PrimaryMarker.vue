c07366a60205c0147435bb16f998869150f4b115
gideon maina <gmainapro@yahoo.com>
2023-05-22 12:49:11 +0300

    add map marker and modify json to show geodata

---
+++ src/components/map-markers/PrimaryMarker.vue
@@ -0,0 +1,60 @@
+<template>
+    <span class="loader">
+  <span class="inner"></span>
+</span>
+</template>
+<style scoped>
+
+.loader {
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  position: relative;
+}
+.loader::after,
+.loader::before {
+  content: '';  
+  box-sizing: border-box;
+  border-radius: 50%;
+  position: absolute;
+  animation: ring 1.5s linear infinite;
+  
+}
+
+.loader::before{
+  width: 24px;
+  height: 24px;
+  border: 4px solid #3be8b0;
+  top:12px;
+  left:12px;
+}
+.loader::after {
+  width: 48px;
+  height: 48px;
+  left: 0;
+  top: 0;
+  border:8px solid #0cb67e; 
+  animation-delay: 0.5s;
+  
+}
+.loader .inner{
+  position:absolute;
+  width:16px;
+  height:16px;
+  background-color:#1fa97b;
+  left:16px;
+  top:16px;
+  border-radius:50%;
+}
+@keyframes ring {
+  0% {
+    transform: scale(0.4);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+
+</style>
--- src/views/SensorsMap.vue
+++ src/views/SensorsMap.vue
@@ -1,31 +1,94 @@
-
 <template>
-    <h1>Map view</h1>
-    <div id="sensorsMapNetwork" class="w-full h-full">
-        
-    </div>
-    </template>
-
-
+  <h1>Map view</h1>
+  <div id="sensorsMapNetwork" class="w-full h-full"></div>
+</template>
 
 <script setup>
-    import { onMounted } from "vue";
-    import "leaflet/dist/leaflet.css";
-    import L from "leaflet";
-    const region={
-        "name":"Nairobi",
-        "lat":-1.3024146,
-        "long":36.7770724
-    }
-    const mapContainer="sensorsMapNetwork"
-    const setMap=()=>{
-       const map= L.map(mapContainer).setView([region.lat,region.long],2.5)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+import { onMounted } from "vue";
+// import Marker from '@/components/map-markers/PrimaryMarker.vue'
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+const region = {
+  name: "Nairobi",
+  lat: -1.3024146,
+  long: 36.7770724,
+};
+const mapContainer = "sensorsMapNetwork";
+const setMap = () => {
+  var map = L.map(mapContainer).setView([region.lat, region.long], 8);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const activeMarkerIcon = L.divIcon({
+    className: "radial-map-marker",
+    html: "<span class='radial-map-marker'><span class='inner'></span></span>",
+    iconSize: [48, 48],
+  });
+
+  L.marker([-1.308009, 36.858899], { icon: activeMarkerIcon }).addTo(map);
+};
+
+onMounted(() => {
+  setMap();
+});
+</script>
+
+<style>
+.radial-map-marker {
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  position: relative;
 }
-    onMounted(()=>{
-setMap()
-    })
-    </script>
+
+.radial-map-marker::after,
+.radial-map-marker::before {
+  content: "";
+  box-sizing: border-box;
+  border-radius: 50%;
+  position: absolute;
+  animation: ring 1.5s linear infinite;
+}
+
+.radial-map-marker::before {
+  width: 24px;
+  height: 24px;
+  border: 4px solid #3be8b0;
+  top: 12px;
+  left: 12px;
+}
+
+.radial-map-marker::after {
+  width: 48px;
+  height: 48px;
+  left: 0;
+  top: 0;
+  border: 8px solid #0cb67e;
+  animation-delay: 0.5s;
+}
+
+.radial-map-marker .inner {
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  background-color: #1fa97b;
+  left: 16px;
+  top: 16px;
+  border-radius: 50%;
+}
+
+@keyframes ring {
+  0% {
+    transform: scale(0.4);
+    opacity: 1;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+</style>
