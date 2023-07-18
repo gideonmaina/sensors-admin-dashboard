ade2982e8dd60527ad821fe645f9d6e24c244e87
Gideon Maina <gmainapro@yahoo.com>
2023-07-18 10:27:36 +0300

    data struct for inactive nodes list

--- src/stores/sensors.store.js
+++ src/stores/sensors.store.js
@@ -8,10 +8,43 @@ export const useNodesStore = defineStore("Nodes", {
     };
   },
 getters:{
+
     inactive_nodes_map(state){
         let map_data=JSON.parse(JSON.stringify(state.inactiveNodes))
         map_data=map_data.map(arr=>{return [arr[0],arr[5],arr[8]] })
         return map_data
+    },
+
+    inactive_all(state){
+      let inactive_nodes=JSON.parse(JSON.stringify(state.inactiveNodes))
+      let nodes=[]
+      let node_struct={
+        "node_id":null,
+        "type":null,
+        "last_active":null,
+        "location":"",
+        "city":"",
+        "country":"",
+        "sensors":[]
+        
+      }
+      inactive_nodes.forEach(arr=>{
+      node_struct.nodeid=arr[0]
+      node_struct.type=arr[3]
+      node_struct.last_active=arr[2][0]
+      node_struct.location=arr[5]
+      node_struct.city=arr[6]
+      node_struct.country=arr[7]
+      node_struct.sensors=arr[8]
+
+      nodes.push(node_struct)
+       
+      })
+
+
+      return nodes
+      
+      
     }
 },
   actions: {
