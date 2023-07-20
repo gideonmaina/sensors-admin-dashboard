3e21f50a9dbc486f3dc5f8358ce3b399e0999572
gideon maina <gmainapro@yahoo.com>
2023-07-20 17:23:58 +0300

    Implement API post request to staging server

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -6,33 +6,33 @@ export const useAuthStore = defineStore("users", {
   state: () => ({
     // user: JSON.parse(localStorage.getItem("user")),
     user: null,
+    token: null,
     returnURL:null,
   }),
   actions: {
-    async login(usrname, pwd) {
+    async login(form_data) {
       try {
+      
+        const response=await axios.post(`${import.meta.env.VITE_SENSORS_API}/login/`,form_data)
+        console.log(response)
+        if(response.status != 200){
+         auth_status = false
+         return auth_status
+        } 
 
-        const response=await axios.get(`${import.meta.env.VITE_API_URL}/users`)
-        
-        response.data.users.forEach(obj => {
-          if(obj.username !=undefined && obj.password != undefined){
-          if(obj.username === usrname && obj.password === pwd) {
-            this.user=obj.username;
-            auth_status=true
-            // console.log(this.user)
-            //localStorage.setItem("user", JSON.stringify(obj));
-            return auth_status;
-          }
+        if(response.data.user.is_staff && response.data.user.is_active){
+          this.user=response.data.user
+          this.token=response.data.token
+          auth_status=true;
+
+          router.push(this.returnUrl || '/');
+          return auth_status
         }
-        });
-       
 
-        
 
        // redirect to previous url or default to home page
        if(!auth_status) return auth_status
 
-       router.push(this.returnUrl || '/');
 
       } catch (error) {
         console.log(error.message);
