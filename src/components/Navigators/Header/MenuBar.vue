901b39a4a9be8e0e9abe1fd6bce06cc595fda773
gideon maina <gmainapro@yahoo.com>
2023-07-29 10:39:53 +0300

    Refactor: Add profile menu component

---
+++ src/components/Navigators/Header/MenuBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ProfileMenu from './ProfileMenu.vue'
+</script>
+
+<template>
+  <v-app-bar class="pl-2" color="#00e5a0" prominent>
+    <RouterLink to="/">
+      <v-avatar>
+        <v-img
+          src="/src/assets/sensors_logo.png"
+          alt="sensors.africa logo"
+        ></v-img>
+      </v-avatar>
+    </RouterLink>
+
+    <v-toolbar-title>sensors.AFRICA staff dashboard</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <div class="text-light-blue-lighten-5">
+      <v-btn
+        variant="text"
+        icon="mdi-gmail"
+        class="text-light-blue-lighten-5"
+      ></v-btn>
+      <v-btn variant="text" icon="mdi-bell"></v-btn>
+      <!-- <v-avatar>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="John"
+        ></v-img>
+      </v-avatar> -->
+      <ProfileMenu/>
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </div>
+  </v-app-bar>
+</template>
---
+++ src/components/Navigators/Header/ProfileMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="profile-container">
+    <div class="profile-image">
+      <v-avatar>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          alt="John"
+        ></v-img>
+      </v-avatar>
+    </div>
+
+    <div class="profile-menu">
+      <ul>
+        <li>Profile</li>
+        <li>Settings</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style >
+.profile-container {
+  position: relative;
+  /* width: inherit;
+  height: inherit; */
+  cursor: pointer;
+  display:inline-block;
+}
+
+.profile-image{
+position: relative;
+display: inline-block;
+}
+.profile-menu {
+  position: absolute;
+  z-index: 2;
+  /* width: inherit;
+  height: inherit; */
+  display: none;
+  background-color: green;
+}
+.profile-container:hover .profile-menu {
+  display: block;
+}
+</style>
