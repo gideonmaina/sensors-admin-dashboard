23502ff714257f9289635fa1c10d9c526330f48c
gideon maina <gmainapro@yahoo.com>
2023-08-04 17:46:49 +0300

    Make side navbar a component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,70 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import MenuBar from "../components/Navigators/Header/MenuBar.vue";
+import SideNav from "../components/Navigators/SideNav.vue";
 </script>
 
 <template>
   <v-app>
     <MenuBar />
     <!-- side navbar -->
-    <v-navigation-drawer
-      color="#111827"
-      name="dashboard-menu"
-      class="text-white"
-    >
-      <v-list density="default" class="flex-column">
-        <div class="subgroup">
-          <v-list-item
-            prepend-icon="mdi-view-dashboard-outline"
-            title="Dashboard"
-            value="dashboard"
-            to="/"
-            class="mt-8"
-            :exact="true"
-          >
-          </v-list-item>
-          <div class="subgroup-items">
-            <v-list-item
-              title="Active Nodes"
-              value="activenodes"
-              to="active-nodes"
-              :exact="true"
-              rounded
-            ></v-list-item>
-            <v-list-item
-              title="Inactive Nodes"
-              value="inactivenodes"
-              to="inactive-nodes"
-              :exact="true"
-              rounded
-            ></v-list-item>
-          </div>
-        </div>
-
-        <v-divider />
-        <v-list-item
-          prepend-icon="mdi-map-search-outline"
-          title="Sensors map"
-          value="sensorsmap"
-          to="/map"
-          :exact="true"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account-hard-hat"
-          title="Field Work"
-          value="fieldwork"
-          to="/map"
-          :exact="true"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-truck-delivery"
-          title="Logistics Work"
-          value="logistics"
-          :exact="true"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <SideNav />
     <!-- /side-navbar -->
 
     <v-main class="flex flex-col my-2 bg-#f2f4f4">
@@ -74,13 +18,3 @@ import MenuBar from "../components/Navigators/Header/MenuBar.vue";
     </v-main>
   </v-app>
 </template>
-
-<style scoped>
-.subgroup {
-  display: flex;
-  flex-direction: column;
-}
-.subgroup-items {
-  align-self: center;
-}
-</style>
