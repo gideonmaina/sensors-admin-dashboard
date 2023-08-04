7d0b91af925b68a069646555a5d95ed9d5505ff0
gideon maina <gmainapro@yahoo.com>
2023-08-04 17:38:40 +0300

    Style router style to exactlink

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,35 +11,34 @@ import MenuBar from "../components/Navigators/Header/MenuBar.vue";
       color="#111827"
       name="dashboard-menu"
       class="text-white"
-      
     >
-      <v-list  density="default" class="flex-column" >
-      
-
-        <div class="dashboard-container">
+      <v-list density="default" class="flex-column">
+        <div class="subgroup">
           <v-list-item
-          prepend-icon="mdi-view-dashboard-outline"
-          title="Dashboard"
-          value="dashboard"
-          to="/"
-          class="mt-8"
-        >
-        </v-list-item>
-        <div class="dashboard-items">
-          <v-list-item
-            title="Active Nodes"
-            value="activenodes"
-            to="active-nodes"
-            
-          ></v-list-item>
-          <v-list-item
-            title="Inactive Nodes"
-            value="inactivenodes"
-            to="inactive-nodes"
-            
-          ></v-list-item>
-         
-        </div>
+            prepend-icon="mdi-view-dashboard-outline"
+            title="Dashboard"
+            value="dashboard"
+            to="/"
+            class="mt-8"
+            :exact="true"
+          >
+          </v-list-item>
+          <div class="subgroup-items">
+            <v-list-item
+              title="Active Nodes"
+              value="activenodes"
+              to="active-nodes"
+              :exact="true"
+              rounded
+            ></v-list-item>
+            <v-list-item
+              title="Inactive Nodes"
+              value="inactivenodes"
+              to="inactive-nodes"
+              :exact="true"
+              rounded
+            ></v-list-item>
+          </div>
         </div>
 
         <v-divider />
@@ -48,17 +47,20 @@ import MenuBar from "../components/Navigators/Header/MenuBar.vue";
           title="Sensors map"
           value="sensorsmap"
           to="/map"
+          :exact="true"
         ></v-list-item>
         <v-list-item
           prepend-icon="mdi-account-hard-hat"
           title="Field Work"
           value="fieldwork"
           to="/map"
+          :exact="true"
         ></v-list-item>
         <v-list-item
           prepend-icon="mdi-truck-delivery"
           title="Logistics Work"
           value="logistics"
+          :exact="true"
         ></v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -74,13 +76,11 @@ import MenuBar from "../components/Navigators/Header/MenuBar.vue";
 </template>
 
 <style scoped>
-
-.dashboard-container{
+.subgroup {
   display: flex;
   flex-direction: column;
 }
-.dashboard-items{
-  align-self:center;
-  }
- 
+.subgroup-items {
+  align-self: center;
+}
 </style>
